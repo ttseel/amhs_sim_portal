a80481ab3f965ba95018ff6c2beacc7e52511671
ttseel <ojs7928@gmail.com>
2021-11-01 08:06:11 +0900

    update 2021.11.1

---
+++ src/component/sendanalysis/ValidateLotTrace.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ValidateLotTrace = () => {
+  return (
+    <div>
+      <h3>
+        <img src="/component/sendanalysis/check.png"></img>
+        <em>Lot Trace 반송량 검증</em>
+      </h3>
+    </div>
+  );
+};
+
+export default ValidateLotTrace;
